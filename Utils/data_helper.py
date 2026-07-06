b40f1536f25d290a3ef8e609fe91e28d2d682d91
JLRonquillo <johnlyndonronquillo127@gmail.com>
2026-07-06 20:32:18 +0800

    Add data helper setters, getters, and resets' functions

--- Utils/data_helper.py
+++ Utils/data_helper.py
@@ -1,6 +1,6 @@
 import json
 
-# Helper for getting difficulties of ESL
+# Helpers for getting difficulties of ESL
 def _get_difficulty_range():
     with open("Data/difficulty_range.json", "r", encoding="utf-8") as file:
         difficulty_range = json.load(file)
@@ -21,6 +21,84 @@ def _getKey_diffculty_range(difficulty_range_value):
     difficulty_range = _get_difficulty_range()
     key = [k for k, v in difficulty_range.items() if v == difficulty_range_value]
     return key
+
+# Helpers for prompt
+# raw prompt
+def _get_raw_prompt():
+    with open("Data/store_data.json", "r", encoding="utf-8") as file:
+        raw_prompt = json.load(file)["raw_prompt"]
+    return raw_prompt
+
+def _reset_raw_prompt():
+    with open("Data/store_data.json", "r", encoding="utf-8") as file:
+        store_data  = json.load(file)
+
+    store_data["raw_prompt"] = {
+            "language": "",
+            "topic": "",
+            "number_of_paragraph": 0,
+            "difficulty_range": {"difficulty_range_from": "", "difficulty_range_to": ""},
+            "number_of_question": 1
+        }
+    with open("Data/store_data.json", "w") as file:
+        json.dump(store_data, file, indent=4)
+# formatted prompt
+def _get_formatted_prompt():
+    with open("Data/store_data.json", "r", encoding="utf-8") as file:
+        formatted_prompt = json.load(file)["formatted_prompt"]
+    return formatted_prompt
+
+def _reset_formatted_prompt():
+    with open("Data/store_data.json", "r", encoding="utf-8") as file:
+        store_data  = json.load(file)
+
+    store_data["formatted_prompt"] = ""
+    with open("Data/store_data.json", "w") as file:
+        json.dump(store_data, file, indent=4)
+# response
+def _get_response():
+    with open("Data/store_data.json", "r", encoding="utf-8") as file:
+        response = json.load(file)["response"]
+    return response
+
+def _reset_response():
+    with open("Data/store_data.json", "r", encoding="utf-8") as file:
+        store_data  = json.load(file)
+
+    store_data["response"] = ""
+    with open("Data/store_data.json", "w") as file:
+        json.dump(store_data, file, indent=4)
+
+# Setters
+def _reset_raw_prompt(language="",topic="",number_of_paragraph=0,drf="",drt="", number_of_question=0):
+    with open("Data/store_data.json", "r", encoding="utf-8") as file:
+        store_data  = json.load(file)
+
+        store_data["raw_prompt"] = {
+            "language": f"{language}",
+            "topic": f"{topic}",
+            "number_of_paragraph": number_of_paragraph,
+            "difficulty_range": {"difficulty_range_from": f"{drf}", "difficulty_range_to": f"{drt}"},
+            "number_of_question": {number_of_question}
+        }
+    with open("Data/store_data.json", "w") as file:
+        json.dump(store_data, file, indent=4)
+
+def _set_formatted_prompt(formatted_prompt):
+    with open("Data/store_data.json", "r", encoding="utf-8") as file:
+        store_data  = json.load(file)
+
+    store_data["formatted_prompt"] = f"{formatted_prompt}"
+    with open("Data/store_data.json", "w") as file:
+        json.dump(store_data, file, indent=4)
+
+def _set_response(response):
+    with open("Data/store_data.json", "r", encoding="utf-8") as file:
+        store_data  = json.load(file)
+
+    store_data["response"] = f"{response}"
+    with open("Data/store_data.json", "w") as file:
+        json.dump(store_data, file, indent=4)
 
 if __name__ == "__main__":
     pass
